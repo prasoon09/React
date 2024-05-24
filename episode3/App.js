f9d3b9788a42af5279006db1cd4dd16038ac11a8
prasoon09 <prasoondgladiator@gmail.com>
2024-05-24 23:21:05 +0530

    rectified boiler plate code

--- episode3/App.js
+++ episode3/App.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 const heading = React.createElement(
     "h1",
     {id:"heading"},
-    "Not rendered"
+    "Heading from React"
 )
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
